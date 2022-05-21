4622dab6a99e380dfdc6e1dcc1b5b14b9c55bbc7
OksanaLisitsyna <lisitsyna.ou@gmail.com>
2022-05-21 07:44:21 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("11", "101")); // 3 + 5 = 8 (1000)
+        System.out.println(bins.mult("11", "101")); // 3 * 5 = 15(1111)
+
+    }
+}
